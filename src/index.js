5d988f394bcb9b5a847d6452cca0d2d7704f6888
zengpang <2516629927@qq.com>
2022-09-28 22:39:37 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+//刷新页面
+let memoizedState=[];//hooks 存放在这个数组
+let cursor=0;//当前 memoizedState 下标
+
+
+function useState(initValue) {
+  memoizedState[cursor] =memoizedState[cursor] || initValue;
+
+  const CurrentCursor=cursor;
+  function setState(newState) {
+    memoizedState[CurrentCursor] = newState;
+    console.log(memoizedState[CurrentCursor],CurrentCursor);
+    render();
+  }
+  return [memoizedState[cursor++], setState];
+}
+
+function useEffect(callback,depArray)
+{
+  const hasNoDeps=!depArray;
+  const deps=memoizedState[cursor];
+  const hasChangeDeps=deps?!depArray.every((el,i)=>el===deps[i])
+  :true;
+  if(hasNoDeps||hasChangeDeps)
+  {
+    callback();
+    memoizedState[cursor]=depArray;
+  }
+  cursor++;
+}
+function App() {
+  const [count, setCount] = useState(0);
+  const [age,setAge]=useState(20);
+  useEffect(()=>{
+    console.log("已经点击按钮");
+  },[count]);
+  return (
+    <div className='App'>
+      <p>You clicked{count} times</p>
+      <button onClick={() => setCount(count + 1)}>点击我</button>
+      <p>当前{age} 年龄</p>
+      <button onClick={() => setAge(age + 1)}>点击我增加年龄</button>
+    </div>
+  );
+}
+const root= ReactDOM.createRoot(document.getElementById('root'));
+function render() {
+  console.log("触发渲染");
+  cursor=0;
+   root.render(
     <App />
-  </React.StrictMode>
+  );
+}
+root.render(
+    <App/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
